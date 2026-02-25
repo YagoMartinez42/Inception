933dda32e45f0b1a54864ee175322b704cf9fee1
samartin <samartin@student.42madrid.com>
2026-02-25 14:53:26 +0100

    .

---
+++ srcs/requirements/wordpress/wpsript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /var/www/html
+chmod +x wp-cli.phar
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+
+php-fpm83 -F
