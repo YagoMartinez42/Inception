a40067d8c4079e4d5dca1e57db3d70d62c2486f3
samartin <samartin@student.42madrid.com>
2026-03-19 13:16:27 +0100

    redundancy removed

--- srcs/requirements/wordpress/wpscript.sh
+++ srcs/requirements/wordpress/wpscript.sh
@@ -6,7 +6,6 @@ chmod +x wp-cli-2.12.0.phar
 
 if [ ! -f wp-config.php ]; then
     PHP="php -d memory_limit=512M"
-    $PHP wp-cli-2.12.0.phar core download --allow-root
     $PHP wp-cli-2.12.0.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
     $PHP wp-cli-2.12.0.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 fi
