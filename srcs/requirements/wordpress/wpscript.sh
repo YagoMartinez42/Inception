c70e0067f8635d0616962912e085733f642a17bb
samartin <samartin@student.42madrid.com>
2026-03-20 15:27:01 +0100

    wpscript

--- srcs/requirements/wordpress/wpscript.sh
+++ srcs/requirements/wordpress/wpscript.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 set -e
 
+
+read_secret() {
+  file="$1"
+  if [ -f "$file" ]; then
+    tr -d '\r\n' < "$file"
+  else
+    echo ""
+  fi
+}
+
+DB_NAME="${MYSQL_DATABASE:-wordpress}"
+DB_USER="${MYSQL_USER:-wpuser}"
+DB_HOST="${MYSQL_HOST:-mariadb}"
+DB_PASS="$(read_secret /run/secrets/db_password)"
+
+WP_URL="${WP_URL:-samartin.42.fr}"
+WP_TITLE="${WP_TITLE:-inception}"
+WP_ADMIN_USER="${WP_ADMIN_USER:-admin}"
+WP_ADMIN_EMAIL="${WP_ADMIN_EMAIL:-admin@admin.com}"
+WP_ADMIN_PASS="$(read_secret /run/secrets/wp_admin_password)"
+
+if [ -z "$DB_PASS" ] || [ -z "$WP_ADMIN_PASS" ]; then
+  echo "Error: db_password and/or wp_admin_password secret is missing/empty"
+  exit 1
+fi
+
 mkdir -p /var/www/localhost/htdocs
 cd /var/www/localhost/htdocs
 
@@ -9,9 +35,22 @@ if [ ! -f wp-settings.php ]; then
 fi
 
 if [ ! -f wp-config.php ]; then
-    PHP="php -d memory_limit=512M"
-    $PHP /usr/local/bin/wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-    $PHP /usr/local/bin/wp core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+  PHP="php -d memory_limit=512M"
+
+  $PHP /usr/local/bin/wp config create \
+    --dbname="${DB_NAME}" \
+    --dbuser="${DB_USER}" \
+    --dbpass="${DB_PASS}" \
+    --dbhost="${DB_HOST}" \
+    --allow-root
+
+  $PHP /usr/local/bin/wp core install \
+    --url="${WP_URL}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${WP_ADMIN_USER}" \
+    --admin_password="${WP_ADMIN_PASS}" \
+    --admin_email="${WP_ADMIN_EMAIL}" \
+    --allow-root
 fi
 
-php-fpm83 -F
+exec php-fpm83 -F
